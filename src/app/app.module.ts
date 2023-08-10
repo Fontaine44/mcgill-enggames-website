c1ff482713c4a12b55bbda152758d7c615b2aa1a
Raphaël Fontaine <raphael.fontaine@mail.mcgill.ca>
2023-08-10 16:18:54 -0400

    Working website with updated dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import * as PlotlyJS from 'plotly.js-dist-min';
-import { PlotlyModule } from 'angular-plotly.js';
-
-PlotlyModule.plotlyjs = PlotlyJS;
 
 @NgModule({
   declarations: [
@@ -19,10 +12,7 @@ PlotlyModule.plotlyjs = PlotlyJS;
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     CoreModule,
-    SharedModule,
-    PlotlyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
